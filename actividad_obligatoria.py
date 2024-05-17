048707333052fd8ed22e9a5c39af8ae8e5a2dc3f
gustavomansilla86 <gustavomansilla1986@hotmail.com>
2024-05-16 22:36:49 -0300

    en proceso

--- actividad_obligatoria.py
+++ actividad_obligatoria.py
@@ -2,46 +2,50 @@
    
 
 def tareas_en_gestion():
-    tareas = []
+  tareas = []
  
-    tareas.insert(0, {"nombre": "limpiar", "descripcion": "limpiar todo", "estado": "sin procesar"})
-    tareas.insert(1, {"nombre": "cocinar", "descripcion": "cocinar algo", "estado": "sin proceso"})
-    tareas.insert(2, {"nombre": "mantenimiento", "descripcion": "poner en punto", "estado": "sin procesar"})
-    tareas.insert(3, {"nombre": "tpm diario","descripcion": "limpieza y orden diario","estado": "sin procesar"})
+  tareas.insert(0, {"nombre": "limpiar", "descripcion": "limpiar todo", "estado": "sin procesar"})
+  tareas.insert(1, {"nombre": "cocinar", "descripcion": "cocinar algo", "estado": "sin proceso"})
+  tareas.insert(2, {"nombre": "mantenimiento", "descripcion": "poner en punto", "estado": "sin procesar"})
+  tareas.insert(3, {"nombre": "tpm diario","descripcion": "limpieza y orden diario","estado": "sin procesar"})
    
-    print("TAREAS A VISIALIZAR:")
+  print("TAREAS A VISIALIZAR:")
 
-    for i, tarea in enumerate(tareas):
-        print(F"Tarea {i+1}:")
-        print(f"Nombre: {tarea['nombre']}")
-        print(f"Descripcion: {tarea['descripcion']}")
-        print(f"Estado: {tarea['estado']}")
-        print()
-    cambiar_tarea = input("desea realizar algun cambio en los estados de las tareas si o no?: ")
-    if cambiar_tarea.lower() == "si":
+  for i, tarea in enumerate(tareas):
+     print((F"Tarea {i+1}: "),(f"Nombre:{tarea['nombre']} "),(f"Descripcion:{tarea['descripcion']} "),(f"Estado: {tarea['estado']}"))
+       
+
+  cambiar_tarea = input("desea realizar algun cambio en los estados de las tareas si o no?: ")
+    
+  if cambiar_tarea.lower() == "si":
      tarea_a_modificar = int(input("Ingrese el número de tarea que desea modificar: ")) - 1
      nuevo_estado = input("Ingrese el nuevo estado de la tarea PROCESADO O SIN PROCESAR: ")
-    tareas[tarea_a_modificar]["estado"] = nuevo_estado
-    print("Tareas actualizadas:")
-    for i, tarea in enumerate(tareas):
+     tareas[tarea_a_modificar]["estado"] = nuevo_estado
+     print("Tareas actualizadas:")
+     for i, tarea in enumerate(tareas):
         print(f"Tarea {i+1}:")
         print(f"Nombre: {tarea['nombre']}")
         print(f"Descripcion: {tarea['descripcion']}")
         print(f"Estado: {tarea['estado']}")
-        print()
-    else:
-       for i, tarea in enumerate(tareas):
+      
+    
+  else:
+       print("no se modifico ninguna tarea")
+     
+   
+  eliminar = print(input("desea eliminar alguna tarea? si o no: "))
+  if eliminar.lower() == "si":
+    numero_de_tarea= print(int(input("eliga el numero de tarea a eliminar:")))-1
+    
+    tareas.pop[numero_de_tarea]
+    print(f"tarea {numero_de_tarea} eliminada")
+
+    for i, tarea in enumerate(tareas):
         print(F"Tarea {i+1}:")
         print(f"Nombre: {tarea['nombre']}")
         print(f"Descripcion: {tarea['descripcion']}")
         print(f"Estado: {tarea['estado']}")
-        print()
+  else:
+       print("no se elimino ninguna tarea")
 
-    eliminar= print(input("desea eliminar alguna tarea? si o no: "))
-    if eliminar.lower() == "si":
-     numero_de_tarea= print(int(input("eliga el numero de tarea a eliminar:")))-1
-     tareas.pop[numero_de_tarea]
-     print(f"tarea {numero_de_tarea} eliminada")
-    else:
-     print("no se elimino ninguna tarea")
 tareas_en_gestion()
